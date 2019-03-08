f97ba100fe2c67d4a19f78f2afeea29ce1ba4a2c
klassynihal <harshaan.n.khan@gmail.com>
2019-03-08 11:49:32 +0530

    reducer done

--- client/src/components/NewInvite/index.js
+++ client/src/components/NewInvite/index.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
+import { util } from "../../util";
 import './NewInvite.css';
 
 class NewInvite extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      invitemail : "",
+      message:"",
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]:e.target.value})
+  }
+
+  handleClick = (e) => {
+    console
+    let email = this.state.invitemail.trim();
+    if(!this.ValidateEmail(email)) return;
+    fetch(`${util.baseURL}/student/invite`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({email})
+    })
+    .then(res=> res.json())
+    .then(data=> {
+      console.log(data);
+      if(!data.error) {
+        this.setState({message:data.msg})
+      } else this.setState({ message: data.error.msg })
+    })
+  }
+
+  ValidateEmail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    return (true)
+  }
+  alert("You have entered an invalid email address!")
+  return (false)
+}
+
   render() {
     return (
       <div className="invite-box">
         <label htmlFor="invite" className="label-box">
           <span className="label-text">Send a new invite</span>
-          <input type="text" id="invite" name="invite" placeholder="Enter an email"/>
+          
+          <input onChange={this.handleChange} ontype="text" id="invite" name="invitemail" placeholder="Enter an email"/>
+          <div>{this.state.message}</div>
+          <button onClick={this.handleClick}>INVITE</button>
         </label>
       </div>
     );
