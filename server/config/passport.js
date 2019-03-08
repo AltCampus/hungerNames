028d0be71d9f74e8d688300b70e2ab7b309415aa
haripallikere <haripallikere93@gmail.com>
2019-03-08 10:39:29 +0530

    fixed passport-stratergy

--- server/config/passport.js
+++ server/config/passport.js
@@ -4,18 +4,26 @@ const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 const Students = mongoose.model('Students');
 
-module.exports = (passport) => {
+module.exports = function(passport) {
   passport.use(new LocalStrategy({
-    usernameField: 'email'
-  }, 
-  function(email, password, done) {
-    Students.findOne({ email }, (err, user) => {
+    usernameField: 'email',passwordField: 'password'}, (email, password, done) => {
+    Students.findOne({ email }, (err, admin) => {
       if (err) { return done(err) }
-      if (!user) { return done(null, false) }
-      if (!bcrypt.compareSync(password, user.password)) {
-        return done(null, false);
-      }
-      return done(null, user);
+      if(!admin) return res.json({
+        message: 'Admin not found'
+      })
+      if(!admin.isAdmin) return res.json({
+        message: 'Admin not found'
+      })
+      if (!admin) { return done(null, false) }
+      bcrypt.compare(password, admin.password, (err, isMatch) => {
+        if (err) throw err;
+        if (isMatch) {
+          return done(null, admin);
+        } else {
+          return done(null, false, { message: "password is incorrect" });
+        }
+      })
     })
   }))
 }
--- server/controller/admin.controller.js
+++ server/controller/admin.controller.js
@@ -1,7 +1,33 @@
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require("../model/User");
+
 module.exports = {
   getAdmiin: (req,res,next) => {
     res.json({
       message: 'welcome admin'
+    })
+  },
+  loginAdmin: (req, res, next) => {
+    passport.authenticate('local',{session: false},(err,user,info) => {
+      if (err || !user) return res.json({
+        message: 'password not matched'
+      })
+      const token = jwt.sign({ user }, 'secret');
+      res.json({ 
+       message: "successfully logged in",
+       token: token
+     }); 
+    }) (req,res, next)
+  },
+  verifyAdmin: (req, res, next) => {
+    res.json({
+      message: 'verified'
+    })
+  },
+  forgetPassword: (req, res, next) => {
+    res.json({
+      message: 'password rest'
     })
   }
 }
