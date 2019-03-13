557e466e0949f87efab52a74f0f1028a09220d28
sachin-me <sachinkumartiwari363@gmail.com>
2019-03-13 22:57:01 +0530

    styling of feedback component

--- client/src/components/StudentFeedback/index.js
+++ client/src/components/StudentFeedback/index.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'; 
 import { getStudentFeedback } from '../../store/actions';
+import './StudentFeedback.scss';
+import ToggleOpen from './ToggleOpen';
 
 class StudentFeedback extends Component {
+  state = {
+    isToggle: false
+  }
+  // componentDidMount = () => {
+  //   const { currentUser } = this.props;
+  //   const currentUserId = currentUser._id;
+  //   this.props.dispatch(getStudentFeedback(currentUserId, (success) => {
+  //     if (success) {
+  //       this.props.history.push(`/student/${currentUserId}`);
+  //     }
+  //   }))
+  // }
 
-  componentDidMount = () => {
-    const { currentUser } = this.props;
-    const currentUserId = currentUser._id;
-    this.props.dispatch(getStudentFeedback(currentUserId, (success) => {
-      if (success) {
-        this.props.history.push(`/student/${currentUserId}`);
-      }
-    }))
+  handleToggleOpen = () => {
+    this.setState({
+      isToggle: true
+    })
+  }
+
+  handleToggleClose = () => {
+    console.log('close it')
+    this.setState({
+      isToggle: false
+    })
   }
 
   render() {
+    const { isToggle } = this.state
     const { currentUser } = this.props;
-    console.log(currentUser);
     return (
-      <div>
-        Hello World!!
+      <div className="feedback-wrapper">
+        <div>
+          {
+            isToggle ? <ToggleOpen handleToggleClose={ this.handleToggleClose } />
+            :
+            <div className="toggle-wrapper" onClick={ this.handleToggleOpen }>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          }
+        </div>
       </div>
     )
   }
